862d94523085a4fe87dd306da13b69666930644c
Artemev Roman <rom1000rom@yandex.ru>
2020-03-03 18:16:21 +0300

    Initial commit.

---
+++ src/main/java/boot/App.java
@@ -0,0 +1,16 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**Класс представляет собой java-конфигурацию Spring Context а также точку входа
+ * в Spring Boot приложение.
+ @author Артемьев Р.А., Синельников М.
+ @version 03.03.2020 */
+@SpringBootApplication
+public class App{
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
