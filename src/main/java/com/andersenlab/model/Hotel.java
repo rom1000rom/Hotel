cf4899e4b7f0ad5a4061948871b6e8aca704b38a
survordie <survordie@ya.ru>
2020-03-04 17:35:59 +0300

    create model

---
+++ src/main/java/com/andersenlab/model/Hotel.java
@@ -0,0 +1,35 @@
+package com.andersenlab.model;
+
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import lombok.Data;
+
+@Entity
+@Table(name = "hotel")
+@Data
+public class Hotel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Version
+  private Integer version;
+
+  @Column(name = "hotel_name")
+  private String hotelName;
+  
+  @OneToMany(mappedBy = "hotelId")
+  private Set<Room> roomSet;
+  
+  @OneToMany(mappedBy = "hotelId")
+  private Set<Service> serviceSet;
+  
+}
