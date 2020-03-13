52ba7e78196b3d5c788499024a5ee1e33a2c176c
survordie <survordie@ya.ru>
2020-03-13 14:09:39 +0300

    add dao for ProvidedRoom and ProvidedFacilities

---
+++ src/main/java/com/andersenlab/dao/ProvidedRoomDao.java
@@ -0,0 +1,12 @@
+package com.andersenlab.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andersenlab.model.ProvidedRoom;
+
+public interface ProvidedRoomDao extends JpaRepository<ProvidedRoom, Long> {
+
+	Page<ProvidedRoom> findAll(Pageable pageable);
+}
